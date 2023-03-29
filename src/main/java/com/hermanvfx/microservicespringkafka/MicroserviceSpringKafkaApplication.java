1cbc53d51440b37ff0183f65100e8aaf4769d4a1
HermanVFX <HermanVFX@outlook.com>
2023-03-29 15:42:09 +0300

    Initial commit

---
+++ src/main/java/com/hermanvfx/microservicespringkafka/MicroserviceSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.hermanvfx.microservicespringkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSpringKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hermanvfx/microservicespringkafka/MicroserviceSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermanvfx.microservicespringkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
